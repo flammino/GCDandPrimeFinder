91154e8f04917a0afe0d98061dd862c756b26a5c
Adam Flammino <flammino@gmail.com>
2017-04-28 09:19:38 -0400

    Updated comments

--- src/Validation.java
+++ src/Validation.java
@@ -1,5 +1,10 @@
 /**
  * Created by Flammino on 4/4/2017.
+ * Checks for empty string
+ */
+
+/**
+ * returns true if string is empty, false if not
  */
 public class Validation {
     public boolean checkEmpty(String s) {
